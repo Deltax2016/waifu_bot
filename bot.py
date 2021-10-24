4b38316d8f1fb15fc68713948b9c7f072dced301
Deltax2016 <deltaxussr@gmail.com>
2021-10-24 14:03:56 +0300

    added token

--- bot.py
+++ bot.py
@@ -10,22 +10,22 @@ phrases = ["Привет, очень рада тебя видеть ", "Прив
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    file_db = open('./notifications/fake_db.py')
+    file_db = open('./fake_db.py')
     db = eval(file_db.read())
     file_db.close()
     db[message.from_user.id] = {"stage": 0}
-    file_db = open('./notifications/fake_db.py','w')
+    file_db = open('./fake_db.py','w')
     file_db.write(str(db))
     text = random.choice(phrases)
     await message.answer(f"{text}{message.from_user.first_name}")
 
 @dp.message_handler(commands="stop")
 async def stop(message: types.Message):
-    file_db = open('./notifications/fake_db.py')
+    file_db = open('./fake_db.py')
     db = eval(file_db.read())
     file_db.close()
-    db[message.from_user.id] = {"notifications": "no"}
-    file_db = open('./notifications/fake_db.py','w')
+    db[message.from_user.id] = {"stage": 10}
+    file_db = open('./fake_db.py','w')
     file_db.write(str(db))
     await message.answer(f"Notifications stopped")
 
